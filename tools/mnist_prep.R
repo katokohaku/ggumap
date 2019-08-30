6c273e6575ccd1e2baf484e05defd94908128969
Satoshi Kato <kato.satoshi.0@gmail.com>
2019-08-30 17:50:55 +0900

    add: prep & umap-learn.rmd

--- tools/mnist_prep.R
+++ tools/mnist_prep.R
@@ -1,26 +1,24 @@
 require(tidyverse)
 require(data.table)
 
-# mnist <- fread("tools/mnist_train.csv", data.table = FALSE)
-# usethis::use_data(mnist)
+#' @source \url{https://github.com/pjreddie/mnist-csv-png}
+
+getwd()
+
+curl.train <- "https://pjreddie.com/media/files/mnist_train.csv"
+dest.train <- "./tools/mnist_train.csv"
+download.file(curl.train, dest.train)
+mnist.train <- fread("./tools/mnist_train.csv", data.table = FALSE)
+
+curl.test <- "https://pjreddie.com/media/files/mnist_test.csv"
+dest.test <- "./tools/mnist_test.csv"
+download.file(curl.test, dest.test)
+mnist.test <- fread("./tools/mnist_test.csv", data.table = FALSE)
+
+mnist <- rbind(mnist.train, mnist.test)
+usethis::use_data(mnist, overwrite = TRUE)
 # save(mnist, file = "data/mnist.rda")
 
 load("data/mnist.rda")
 str(mnist, 0)
-df.u <- mnist[, -1]
 
-p = 30
-n = nrow(df.u)
-
-system.time(
-  ri <- Rtsne::Rtsne(unique(df.u), perplexity = p)
-)
-kld <- last(ri$itercosts)
-tmp <- c(
-  perplexity = p,
-  KLdist     = kld,
-  pen.KLdist = kld + log(n) * p/n)
-
-# KL <- rbind(KL, tmp)
-cat(tmp, "\n")
-plot(ri$Y[,1], ri$Y[,2])
